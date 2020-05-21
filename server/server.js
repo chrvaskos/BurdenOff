eb4d6a1df6925beb48eb9a411f613c1b409f13a7
chrvaskos <chrvaskos@gmail.com>
2020-05-21 17:10:07 +0300

    another line

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const apiRouter = require('./routes');
+const express = require("express");
+const apiRouter = require("./routes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/posts' , apiRouter)
+app.use("/api/posts", apiRouter);
 
-
-app.listen(process.env.PORT || '5000', () => {
-
-    console.log(`Geia soy ${process.env.PORT || '5000' }`);
+app.listen(process.env.PORT || "5000", () => {
+  console.log(`Geia soy ${process.env.PORT || "5000"}`);
 });
