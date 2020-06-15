f53c840cba40fa59d7367ec446795114a8d19142
chrvaskos <chrvaskos@gmail.com>
2020-06-15 22:26:30 +0300

    tiny changes

--- client/src/components/Session.js
+++ client/src/components/Session.js
@@ -98,13 +98,9 @@ class Session extends Component {
     for (var i = 0; i < btns.length; i++) {
       btns[i].addEventListener("click", function () {
         var current = document.getElementsByClassName("active");
-
-        // If there's no active class
         if (current.length > 0) {
           current[0].className = current[0].className.replace(" active", "");
         }
-
-        // Add the active class to the current/clicked button
         this.className += " active";
       });
     }
